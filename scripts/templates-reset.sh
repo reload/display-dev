7d1b39d80cf14365f2231897e840c61f5344004a
Kasper Birch <kasperbirch1@gmail.com>
2022-04-21 13:09:37 +0200

    This commit improves the development experience for developing templates
    We
    - Introduce the nessecary tasks and reset-files for resetting the template development containers
    - Import all templates during reset - we use the versions hosted on github for now
    - Prepares the setup to allow the api-server to import templates from a local container

---
+++ scripts/templates-reset.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Reset the templates develop setup
+#
+# The script must be executed with a clone of display-templates as current
+# working dir.
+
+set -euo pipefail
+IFS=$'\n\t'
+
+docker compose down -v || true
+
+cp ../../etc/templates_docker-compose.override.yml ./docker-compose.override.yml
+
+docker-compose run node yarn install
+docker compose up -d
